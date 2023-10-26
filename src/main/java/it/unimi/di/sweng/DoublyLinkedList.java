150e1c212f5a9d1f6506b1e93747c260e4e462d7
Vale <>
2023-10-26 10:10:59 +0200

    GREEN: passa anche il secondo test, ho aggiunto la classe interna Node per permettere di aggiungere e togliere nodi

--- src/main/java/it/unimi/di/sweng/DoublyLinkedList.java
+++ src/main/java/it/unimi/di/sweng/DoublyLinkedList.java
@@ -1,19 +1,46 @@
 package it.unimi.di.sweng;
 
+
 import java.util.LinkedList;
+import java.util.Objects;
 
 public class DoublyLinkedList<Integer>{
-
+    private Node head;
+    private Node tail;
 
     public DoublyLinkedList(){
-
+        this.head=null;
+        this.tail=null;
     }
-    public void push(Integer n){
-
+    public void push(int num){
+        Node nodo = new Node(num);
+        if (Objects.isNull(head)){
+            head = nodo;
+            tail = nodo;
+        }else{
+            nodo.prev=tail;
+            tail.next=nodo;
+            tail=nodo;
+        }
     }
 
     public int pop(){
-        return 10;
+        int value = tail.num;
+        if (head==tail){
+            head=null;
+        }
+        tail= tail.prev;
+        return value;
+    }
+
+    public static class Node {
+         final int num;
+         Node next = null;
+         Node prev = null;
+
+        public Node(int num){
+            this.num=num;
+        }
     }
 
 }
